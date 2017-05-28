1c0b9e4c3d2b682a6462c45835dbfcd2a7b01810
GangstaCoder <GangstaCoder@users.noreply.github.com>
2017-05-28 23:12:55 +0300

    lswebhomework4

---
+++ lesson4.js
@@ -0,0 +1,15 @@
+/*2. Link it to a JavaScript file that you will edit
+
+## Using the JavaScript file *only* (not using a CSS file) ##
+
+1. Change the background color of the entire document to red
+2. Change the background color of just the "about me" portion to blue
+3. Change the font-style of the entire document to sans-serif
+4. Add your nickname 
+5. Add your favorites
+6. Add your hometown
+
+## Extra Credit (still only using JavaScript) ##
+
+1. Iterate through each li and change the class to "listitem". Add a style tag that sets a rule for "listitem" to make the color red.
+2. Create a new `img` element and set its src attribute to a picture of you. Append that element to the page.*/
