7d6972dd5c83c623eacc3ac59b7a05c021e79778
GangstaCoder <GangstaCoder@users.noreply.github.com>
2017-05-29 23:23:45 +0300

    lswebhomework4

--- lesson4.js
+++ lesson4.js
@@ -12,6 +12,11 @@
 ## Extra Credit (still only using JavaScript) ##
 
 1. Iterate through each li and change the class to "listitem". Add a style tag that sets a rule for "listitem" to make the color red.
+<ul>
+	<li>Nickname: <span id="nickname"></span>
+	<li>Favorites:  <span id="favorites"></span>
+	<li>Hometown: <span id="hometown"></span>
+</ul> 
 2. Create a new `img` element and set its src attribute to a picture of you. Append that element to the page.*/
 document.getElementById("blue").style.color = "blue";
 document.body.style.backgroundColor = "red";
@@ -22,7 +27,21 @@ document.getElementById("nickname").innerHTML = "Elbonzorino";
 document.getElementById("favorites").innerHTML = "lime";
 document.getElementById('hometown').innerHTML = "Manzini";
 
+function myFunction() {
+    for (i = 0; i < 2; i++){
+    var list = document.getElementsByTagName("ul");getElementById("");
+    list.getElementById("").appendChild = "listitem";
+    list.getElementById("listitem").style.color = "red";
+}};
 
+
+/*function myFunction() {
+    for (i = 0; i < 2; i++){
+    var list = document.getElementsByTagName("span");
+    list.getElementsById("").innerHTML = "listitem";
+    document.getElementById("listitem").style.color = "red";
+}};
+*/
 
 var x = document.createElement("IMG");
 x.setAttribute("src", "truck.jpg");
